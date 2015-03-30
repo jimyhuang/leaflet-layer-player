47862d28f9e642d68a92b5ce1b30e39023195566
Jimmy Huang <jimmy@netivism.com.tw>
2015-03-31 00:51:33 +0800

    Add chart support, add title

--- src/leaflet-layer-player.js
+++ src/leaflet-layer-player.js
@@ -7,26 +7,43 @@ L.Control.LayerPlayer = L.Control.extend({
     maxZoom: 13,
     attribution: '',
     playInterval: 2000,
-    loadingDelay: 3500
+    overlay: true,
+    loadingDelay: 3500,
+    chart: false
   },
   
   initialize: function(options) {
     L.Util.setOptions(this, options);
     this._idx = 0;
-    this._rid = Math.random().toString(36).replace(/[^a-z] + /g, '').substr(0, 5);;
+    this._length = this.options.slides.rows.length;
+    this._rid = Math.random().toString(36).replace(/[^a-z] + ./g, '').substr(-5);;
+    this._layers = [];
     this.$overlay = jQuery();
   },
 
   onAdd: function(map) {
     this.options.map = map;
     this.$map = $(map._container);
-    this.addOverlay();
+    this.addTitle();
+    if (this.options.overlay) {
+      this.addOverlay();
+    }
     this.addLayers();
 
     var div = L.DomUtil.create('div', 'controlbar', this._container);
     var $control = this.addControlBar(div);
     this.changeLayer(0);
+
+    if (this.options.chart) {
+      this.addChart();
+    }
     return $control[0];
+  },
+
+  onRemove: function (map) {
+    for (var i = 0; i < this._length; i++) {
+      map.removeLayer(this.layers[i]);
+    }
   },
 
   addOverlay: function() {
@@ -49,6 +66,12 @@ L.Control.LayerPlayer = L.Control.extend({
     }, this.options.loadingDelay);
   },
 
+  addTitle: function() {
+    var $title = $('<div class="slide-title"></div>');
+    $title.html(this.options.slides.subject);
+    $title.prependTo(this.$map);
+  },
+
   addControlBar: function(div) {
     var $overlay = this.$overlay;
     var $control = $(div);
@@ -69,13 +92,13 @@ L.Control.LayerPlayer = L.Control.extend({
     // jquery ui slider
     var $slider = $('<div id="slider-' + this._rid + '" class="slider"></div>');
     var labels = [];
-    for(var i = 0; i < this.options.slides.rows.length; i++){
+    for (var i = 0; i < this._length; i++){
       labels.push(this.options.slides.rows[i].date);
     }
     this.$slider = $slider.slider({
       value: 0,
       min: 0,
-      max: obj.options.slides.rows.length - 1,
+      max: obj._length - 1,
       step: 1
     })
     .slider("pips", {
@@ -85,6 +108,7 @@ L.Control.LayerPlayer = L.Control.extend({
     .on("slidechange", function(e, ui) {
       obj._changeLayer(ui.value);
     });
+    $slider.width(this.$map.width() - 280);
     $control.append($slider);
 
     $next.click(function(e) {
@@ -102,10 +126,56 @@ L.Control.LayerPlayer = L.Control.extend({
     return $control;
   },
 
+  addChart: function() {
+    this.$chart = $('<svg class="chart" id="chart-' + this._rid + '"></svg>');
+    this.$chart.appendTo(this.$map);
+    var length = this._length;
+    if (typeof d3 !== 'undefined') {
+      var height = this.$map.height() * 0.3;
+      var bwidth = (this.$slider.width()/(length - 1));
+      var width = bwidth * length;
+      var left = parseInt(this.$chart.css('left')) - bwidth/2;
+      this.$chart.css('left', left);
+
+      var x = d3.scale.ordinal()
+        .domain(d3.range(this._length))
+        .rangeBands([0, width]);
+      var y = d3.scale.linear()
+        .range([height, 0]);
+      var rows = this.options.slides.rows;
+
+      for (var i = 0; i < this._length; i++) {
+        rows[i].i = i;
+      }
+      d3.select('#chart-' + this._rid)
+        .attr('width', width)
+        .attr('height', height);
+      d3.select('#chart-' + this._rid).selectAll(".bar")
+        .data(this.options.slides.rows)
+        .enter()
+        .append('rect')
+        .attr("class", "bar")
+        .attr('x', function(d) { return x(d.i); })
+        .attr("width", bwidth)
+        .attr("y", function(d) { return y(d.data/100); })
+        .attr("height", function(d) { return height - y(d.data/100); })
+      d3.select('#chart-' + this._rid).selectAll(".text")
+        .data(this.options.slides.rows)
+        .enter()
+        .append('text')
+        .text(function(d) { return d.data + '%'; })
+        .attr("class", "text")
+        .attr('x', function(d) { return x(d.i) + bwidth/2 - 22; })
+        .attr("width", bwidth)
+        .attr("y", function(d) { return height - 30; })
+        .attr("height", 15)
+    }
+  },
+
   addLayers: function(i) {
     var slide, server, rgbview;
     var attribution = this.options.attribution;
-    for(var i = 0; i < this.options.slides.rows.length; i++){
+    for (var i = 0; i < this._length; i++){
       slide = this.options.slides.rows[i];
 
       // random server
@@ -116,6 +186,7 @@ L.Control.LayerPlayer = L.Control.extend({
         maxZoom: this.options.maxZoom
       });
       rgbview.addTo(this.options.map);
+      this._layers.push(rgbview);
     }
   },
   
@@ -135,12 +206,11 @@ L.Control.LayerPlayer = L.Control.extend({
     current.hide();
     current.css('z-index', 999);
     current.fadeIn(300);
-    this.$map.find('.slide-legend h3').html(slide.date);
     this.$map.find('.leaflet-tile-pane .leaflet-layer').not('.current').css('z-index', 0);
   },
 
   next: function() {
-    if(this._idx + 1 < this.options.slides.rows.length){
+    if(this._idx + 1 < this._length){
       this.$slider.slider("value", this._idx + 1);
     }
   },
@@ -157,7 +227,7 @@ L.Control.LayerPlayer = L.Control.extend({
     var layeridx = i++;
     this.$overlay.fadeOut();
     this.options.interval = setInterval(function() {
-      if(i >= obj.options.slides.rows.length ){
+      if(i >= obj._length ){
         i = 0;
         clearInterval(obj.options.interval);
         return;
